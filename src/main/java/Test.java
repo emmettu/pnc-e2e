5631e85ac280b7bbe98237203ed6909cd51d7e6e
Emmett Underhill <eunderhi@redhat.com>
2015-07-28 10:14:13 -0400

    Removed old testing stuff from main

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,8 @@
-import ui.tests.ButtonClickTest;
-import ui.tests.ProductsTest;
 
 /**
  * Created by eunderhi on 24/07/15.
  */
 public class Test {
     public static void main(String[] args) {
-        ButtonClickTest.test();
-        ProductsTest.test();
     }
 }
