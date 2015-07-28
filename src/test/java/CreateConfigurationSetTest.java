ea3ed65f0d5cb582bfb8c02af882bd62c5a45250
Emmett Underhill <eunderhi@redhat.com>
2015-07-28 10:09:28 -0400

    Setting up junit test to create build config set

--- src/test/java/CreateConfigurationSetTest.java
+++ src/test/java/CreateConfigurationSetTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 /**
  * Created by eunderhi on 28/07/15.
  */
-public class CreateConfigurationSetTest {
+public class CreateConfigurationSetTest extends UITest{
+    @Test
+    public void CreateConfigurationSet() {
+        tester.clickLink("Configurations");
+        tester.clickLink("Build Configuration Sets");
+        tester.clickButton("Create Build Configuration Set");
+        tester.insertInput("name", "teeest");
+        tester.takeScreenshot();
+    }
 }
