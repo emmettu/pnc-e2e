3bd3f0c4cd3cf71a938d923826f9b104488adbee
Emmett Underhill <eunderhi@redhat.com>
2015-07-29 15:14:11 -0400

    Fixing indent in pom

--- src/test/java/SelectTest.java
+++ src/test/java/SelectTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 /**
  * Created by eunderhi on 29/07/15.
  */
-public class SelectTest {
+public class SelectTest extends UITest {
+    @Test
+    public void selectItem() {
+        tester.clickLink("Configurations");
+        tester.clickLink("Build Configurations");
+        tester.clickButton("Create Configuration");
+        tester.clickSelect("createCtrl.data.projectId", "1");
+        tester.takeScreenshot();
+    }
 }
