2beba3eb6546b7b5aa2a249a8aa891bbaef9c38f
Emmett Underhill <eunderhi@redhat.com>
2015-08-06 16:28:57 -0400

    Release extends Milestonetest
    Release needs a milestone so it now extends milestone.

--- src/test/java/MilestoneVersionTest.java
+++ src/test/java/MilestoneVersionTest.java
@@ -1,14 +1,14 @@
-import org.junit.Test;
+import org.junit.Before;
 
 /**
  * Created by eunderhi on 29/07/15.
  */
-public class MilestoneTest extends VersionTest {
+public class MilestoneVersionTest extends VersionTest {
     private static final String VERSION_NUMBER = "1.12";
     private static final String START_DATE = "1994/10/05";
     private static final String RELEASE_DATE = "1997/07/04";
 
-    @Test
+    @Before
     public void createMilestone() {
         tester.clickButton("Create Milestone");
         tester.insertInput("version", VERSION_NUMBER);
