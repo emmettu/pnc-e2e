faf94b55e4f4bb51dd1d22405d774ac2c66b7f48
Emmett Underhill <eunderhi@redhat.com>
2016-03-11 14:26:58 -0500

    Remove old phantomjs configuration
    Because the e2e tests run with firefox and xvfb there's no need to
    have phantomjs configuration.

--- src/main/java/operators/SetUpOperator.java
+++ src/main/java/operators/SetUpOperator.java
@@ -16,22 +16,8 @@ public class SetUpOperator extends Operator {
 
     private Credentials creds = new Credentials();
     private static final int LOAD_WAIT_TIME = 5;
-    private String phantomjsPath = creds.getPhantomjsPath();
 
     public void setUp() {
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("takesScreenshot", true);
-        caps.setCapability(
-                PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-                phantomjsPath
-        );
-        String[] phantomArgs = new  String[] {
-                "--webdriver-loglevel=NONE",
-                "--ssl-protocol=any"
-        };
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomArgs);
-        //driver = new PhantomJSDriver();
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(LOAD_WAIT_TIME, TimeUnit.SECONDS);
         driver.manage().window().setSize(new Dimension(1024, 768));
